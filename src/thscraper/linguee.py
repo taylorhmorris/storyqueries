94cec741fa96f215e7b072037bfeecedf72067ef
taylorhmorris <18409399+taylorhmorris@users.noreply.github.com>
2025-03-25 12:34:43 -0500

    feat: create linguee

---
+++ src/thscraper/linguee.py
@@ -0,0 +1,71 @@
+"""Linguee query and parser"""
+
+# pylint: disable=too-few-public-methods
+import os
+from typing import no_type_check
+
+from bs4 import BeautifulSoup
+from query_and_cache.json_cache import JSONCache
+from query_and_cache.parser import Parser
+from query_and_cache.query import Query, QueryConfig
+from requests import Response
+
+
+class LingueeParser(Parser):
+    """Parser for Linguee API response"""
+
+    @no_type_check
+    def parse_examples(self, soup: BeautifulSoup) -> list[dict[str, str]]:
+        """Parse the examples from Linguee"""
+        example_spans = soup.find_all("span", class_="tag_e")
+        examples = []
+        for span in example_spans:
+            source = span.find("span", class_="tag_s").text
+            translation = span.find("span", class_="tag_t").text
+            examples.append({"source": source, "translation": translation})
+        return examples
+
+    @no_type_check
+    def parse_expressions(self, soup: BeautifulSoup) -> list[dict[str, str]]:
+        """Parse the expressions from Linguee"""
+        try:
+            expression_group = soup.find_all(class_="example_lines")[-1].text.split(
+                "\n\n\n\n"
+            )
+        except IndexError:
+            expression_group = []
+        expressions = []
+        for expression in expression_group:
+            if expression != "":
+                pairs = expression.split(":")[-1].strip("\n").split("—")
+                if len(pairs) >= 2:
+                    expressions.append(
+                        {
+                            "expression": pairs[0],
+                            "translation": pairs[1].replace("\n", ""),
+                        }
+                    )
+        return expressions
+
+    def parse(self, raw: Response) -> dict[str, list[dict[str, str]]]:
+        """Parse the data from Linguee"""
+        soup = BeautifulSoup(raw.text, "html.parser")
+        examples = self.parse_examples(soup)
+        expressions = self.parse_expressions(soup)
+        results = {"examples": examples, "expressions": expressions}
+        return results
+
+
+class QueryLinguee(Query):
+    """Query Configured to send queries to Linguee"""
+
+    def __init__(self, cache_path: str = "cache") -> None:
+        url_root = "https://linguee.com/english-french/search"
+        url = url_root + "?source=french&query={search_string}"
+        parser = LingueeParser()
+        cache = JSONCache(os.path.join(cache_path, "linguee"))
+        config: QueryConfig = {
+            "parser": parser,
+            "cache": cache,
+        }
+        super().__init__(url, config)
---
+++ tests/test_linguee.py
@@ -0,0 +1,33 @@
+"""Test the QueryLinguee class"""
+
+from unittest import mock
+
+from thscraper.linguee import QueryLinguee
+
+
+def test_linguee_creates():
+    """Test if the QueryLinguee class is created correctly"""
+    linguee = QueryLinguee()
+    root = "https://linguee.com/english-french/search"
+    query = "?source=french&query={search_string}"
+    assert linguee.requester.base_url == root + query
+
+
+def test_linguee_parse():
+    """Test if the parse method works correctly"""
+    linguee = QueryLinguee()
+    mock_response = mock.MagicMock()
+    mock_response.text = """
+        <html><body>
+            <span class="tag_e"><span class="tag_s">ExSource</span>
+            <span class="tag_t">ExTranslation</span></span>
+        </body></html>
+        """
+    linguee.parser.parse(mock_response)
+    examples = [
+        {"source": "ExSource", "translation": "ExTranslation"},
+    ]
+    assert linguee.parser.parse(mock_response) == {
+        "examples": examples,
+        "expressions": [],
+    }
